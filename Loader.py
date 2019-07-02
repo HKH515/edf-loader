0913fd1610e4aac47f0f27cb07adef45bb05a211
hannes <hanneskr4@gmail.com>
2019-07-02 14:13:57 +0000

    added comments

--- Loader.py
+++ Loader.py
@@ -21,7 +21,17 @@ class Loader(keras.utils.Sequence):
 
     def __init__(self, path, timestep=50):
         """
+        Reads in a folder `path`, and loads all files that have the extension .npz, these files are read as numpy arrays using np.load().
+        These arrays will be split into x_train, x_test, y_train, y_test.
+
+        x_train and x_test arrays are split into 3 dimensions, along a batch, along a timestep, and along sample
+        batch (is an ndarray): selection of timesteps, meant to be used for training a neural network.
+        timestep (is an ndarray): number of samples to be gathered together to form a single timestep
+        sample (is a number): a single datapoint
+
         segment_size is the number of seconds allocated to each segment of the signal, in seconds
+
+        further explanation of the terms batch, and timestep, can be found here: https://machinelearningmastery.com/use-timesteps-lstm-networks-time-series-forecasting/
         """
         self.path = path
         if not os.path.exists(self.path):
@@ -38,10 +48,15 @@ class Loader(keras.utils.Sequence):
 
 
                     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+                    # remove dimensions that have the length 1
                     x_train = np.squeeze(x_train)
                     x_test = np.squeeze(x_test)
                     y_train = np.squeeze(y_train)
                     y_test = np.squeeze(y_test)
+
+                    # for better labels, convert to int
+                    y_train = y_train.astype(int)
+                    y_test = y_test.astype(int)
                     #print(x_train.shape)
                     #print("----------------------------------------")
                     x_train = samplify(x_train, self.timestep)
